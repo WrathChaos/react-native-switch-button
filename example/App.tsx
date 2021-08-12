9d9dbc6abe9085332b82e0fa001deac2b989e0af
FreakyCoder <kurayogun@gmail.com>
2021-08-13 00:10:52 +0300

    Logo is updated

--- example/App.tsx
+++ example/App.tsx
@@ -13,16 +13,45 @@ import SwitchButton from "@freakycoder/react-native-switch-button";
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1, margin: 32 }}>
-      <SwitchButton
-        inactiveImageSource={require("./assets/notification.png")}
-        activeImageSource={require("./assets/notification-3.png")}
-        text="Notification"
-        textStyle={{
-          color: "#757575",
-          fontWeight: "600",
+      <Text>App Setting</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          width: "100%",
+          justifyContent: "space-evenly",
         }}
-        onPress={(isActive: boolean) => console.log(isActive)}
-      />
+      >
+        <SwitchButton
+          inactiveImageSource={require("./assets/notification.png")}
+          activeImageSource={require("./assets/notification-3.png")}
+          text="Notification"
+          textStyle={{
+            color: "#757575",
+            fontWeight: "600",
+          }}
+          onPress={(isActive: boolean) => console.log(isActive)}
+        />
+        <SwitchButton
+          inactiveImageSource={require("./assets/notification.png")}
+          activeImageSource={require("./assets/notification-3.png")}
+          text="Notification"
+          textStyle={{
+            color: "#757575",
+            fontWeight: "600",
+          }}
+          onPress={(isActive: boolean) => console.log(isActive)}
+        />
+        <SwitchButton
+          inactiveImageSource={require("./assets/notification.png")}
+          activeImageSource={require("./assets/notification-3.png")}
+          text="Notification"
+          textStyle={{
+            color: "#757575",
+            fontWeight: "600",
+          }}
+          onPress={(isActive: boolean) => console.log(isActive)}
+        />
+      </View>
     </SafeAreaView>
   );
 };
